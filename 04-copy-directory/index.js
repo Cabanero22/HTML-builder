8ac1d833b2ff9173cb4740a52d19b6d30fa76898
Cabanero22 <dimancho1@gmail.com>
2024-01-21 17:59:10 +0500

    feat: complete task number 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+
+const filesPass = path.join(__dirname, "files");
+const filesCopyPass = path.join(__dirname, "files-copy");
+
+fs.mkdir(filesCopyPass, {recursive: true}, (err) => {
+    if (err) return console.log(err);
+})
+
+fs.readdir(filesCopyPass, (err, files) => {
+    if(err) return console.log(err);
+    if(!files) return;
+    if (files) {
+        files.forEach((file) => {
+            const copyFilePass = path.join(filesCopyPass, `${file}`);
+            fs.unlink(copyFilePass, (err) => {
+                if (err) return console.log(err);
+            })
+        })
+    }
+})
+
+fs.readdir(filesPass, { withFileTypes: false }, (err, files) => {
+    if(err) return console.log(err);
+    files.forEach( file => {
+        fs.copyFile(path.join(filesPass, `${file}`), path.join(filesCopyPass, `${file}`), (err) => {
+            if(err) return console.log(err);
+            console.log(`File ${file} create`)
+        })
+    });
+})
+
+
